f1cb826b02282ae742c3f7a6c99ed68e1c202fc9
davidandreuroqueta <david.andreu.roqueta@gmail.com>
2024-05-04 10:36:02 +0200

    Create embeddings

---
+++ settings_pan-clef.py
@@ -0,0 +1,12 @@
+'''
+definitions of important setting variables (resources etc.)
+create a local settings.py (git ignored) with these variables and their appropriate values
+when introducing new variables, add them to this file with empty or default values and commit
+'''
+
+# DATASETS
+TRAIN_DATASET_ES="Dataset-Oppositional/training/dataset_es_train.json"
+TRAIN_DATASET_EN="Dataset-Oppositional/training/dataset_en_train.json"
+
+TEST_DATASET_EN ="Dataset-Oppositional/test/dataset_en_official_test_nolabels.json"
+TEST_DATASET_ES ="Dataset-Oppositional/test/dataset_en_official_test_nolabels.json"
